2af3285cc3d1ded15ca157047648f35fee11b4dd
mitchwenman <mitchwenman@gmail.com>
2014-04-19 11:31:58 +1000

    added stub for department view controller

---
+++ SOFA/Controllers/DepartmentViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOFA.Controllers
+{
+    public class DepartmentViewController : Controller
+    {
+        //
+        // GET: /DepartmentView/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateEdit(int id = 0)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEdit()
+        {
+            return View();
+        }
+	}
+}
