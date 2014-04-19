6d77f5009921a6d05929502d870d6c8e6d39dde4
mitchwenman <mitchwenman@gmail.com>
2014-04-19 19:31:15 +1000

    ready for department view

--- SOFA/Controllers/DepartmentViewController.cs
+++ SOFA/Controllers/DepartmentViewController.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SOFA.Models;
 
 namespace SOFA.Controllers
 {
     public class DepartmentViewController : Controller
     {
+        private DBContext db = new DBContext();
         //
         // GET: /DepartmentView/
         public ActionResult Index()
         {
-            return View();
+            return View(db.Departments.ToList());
         }
 
         public ActionResult CreateEdit(int id = 0)
