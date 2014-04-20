a7ead5b7ed58e17457a1e56f2e0c4716e8b6389f
mitchwenman <mitchwenman@gmail.com>
2014-04-20 12:34:15 +1000

    added timetable display name annotations

--- SOFA/Models/Timetable.cs
+++ SOFA/Models/Timetable.cs
@@ -11,12 +11,16 @@ namespace SOFA.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name="Code")]
         public String TimetableCode { get; set; }
 
+        [Display(Name="Name")]
         public String TimetableIdentifier { get; set; }
 
+        [Display(Name="Expiry Date")]
         public DateTime ExpiryDate { get; set; }
 
+        [Display(Name="Active Date")]
         public DateTime ActiveDate { get; set; }
 
         public virtual ICollection<Line> Lines { get; set; }
