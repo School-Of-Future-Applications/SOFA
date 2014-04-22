92315d498c814fcc531cbb3b5a67ba78fb675241
mitchwenman <mitchwenman@gmail.com>
2014-04-22 13:12:16 +1000

    added scaffolded baseclass create view

---
+++ SOFA/Controllers/ClassBaseController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SOFA.Models;
+
+namespace SOFA.Controllers
+{
+    public class ClassBaseController : Controller
+    {
+        //
+        // GET: /ClassBase/
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        //
+        // GET: /ClassBase/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /ClassBase/Create
+        public ActionResult Create(int courseID)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ClassBase/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /ClassBase/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ClassBase/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /ClassBase/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ClassBase/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
