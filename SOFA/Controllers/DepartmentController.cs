da2a34435854af0bd07bfecdbf9163f945c2df40
Thomas Miller <thomas@tlm.id.au>
2014-04-23 16:23:23 +1000

    Added department list function.
    Still not 100% complete. This is just to commit some work.
    Update RouteConfig for default root route into dashboard.
    Renamed DepartmentController as the original one did not work.

---
+++ SOFA/Controllers/DepartmentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SOFA.Models;
+
+namespace SOFA.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private DBContext db = new DBContext();
+       
+        public ActionResult Index()
+        {
+            ViewBag.NavItem = "Department & Courses";
+            return View(db.Departments.ToList());
+        }
+
+        public ActionResult CreateEdit(int id = 0)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEdit()
+        {
+            return View();
+        }
+	}
+}
