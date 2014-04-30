5b65a8d835a3378b60882673c7097d2d91a52655
mitchwenman <mitchwenman@gmail.com>
2014-04-30 13:44:16 +1000

    list of timetable names

--- SOFA/Controllers/TimetabledClassController.cs
+++ SOFA/Controllers/TimetabledClassController.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SOFA.Models;
 
 namespace SOFA.Controllers
 {
     public class TimetabledClassController : Controller
     {
+        private DBContext db = new DBContext();
         //
         // GET: /TimetabledClass/
-        public ActionResult Index()
+        public ActionResult Index(int baseClassID = 1) //default value for debugging purposes only
         {
+            List<String> timetableNames = new List<String>
+            {
+                "All",
+                "Unassigned"
+            };
+            /* Append all the timetable names to the list */
+            var timetables = db.Timetables.ToList();
+            timetables.ForEach(t => timetableNames.Add(t.TimetableIdentifier));
+
             return View();
         }
+
+
 	}
 }
