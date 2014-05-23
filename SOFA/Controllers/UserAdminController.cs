78c6a773a33e2953293bb526f1d54d3815320b95
Thomas Miller <thomas@tlm.id.au>
2014-05-24 08:15:02 +1000

    Implements and closes #46

---
+++ SOFA/Controllers/UserAdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SOFA.Infrastructure;
+
+namespace SOFA.Controllers
+{
+    public class UserAdminController : DashBoardBaseController
+    {
+        // GET: /UserAdmin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
