72a105a042de47264e6d4bcf3c9512aba76cf88d
mitchwenman <mitchwenman@gmail.com>
2014-08-03 08:22:49 +1000

    created controllers

---
+++ SOFA/Controllers/FormController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SOFA.Infrastructure;
+
+namespace SOFA.Controllers
+{
+    public class FormController : DashBoardBaseController
+    {
+        //
+        // GET: /Form/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateEdit(String FormID = null)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEdit(Form form)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Form/Delete/5
+        public ActionResult Delete(String FormID)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Form/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeletePost(String FormID)
+        {
+            return View();
+        }
+	}
+}
---
+++ SOFA/Controllers/SectionController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SOFA.Infrastructure;
+
+namespace SOFA.Controllers
+{
+    public class SectionController : DashBoardBaseController
+    {
+        //
+        // GET: /Section/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Section/Create
+        public ActionResult CreateEdit(String SectionID = null)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Section/Create
+        [HttpPost]
+        public ActionResult CreateEdit(Section section)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        //
+        // GET: /Section/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Section/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
