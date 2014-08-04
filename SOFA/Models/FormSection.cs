2bcb9fedde9907d0e38df9c514513d679d51fdd2
Thomas Miller <thomas@tlm.id.au>
2014-08-05 09:38:19 +1000

    Update model and start of views.

--- SOFA/Models/FormSection.cs
+++ SOFA/Models/FormSection.cs
@@ -4,12 +4,21 @@ using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 
 namespace SOFA
 {
     public class FormSection
     {
+        [Key, Column(Order = 1)]
+        public String FormId { get; set; }
+
+        [Key, Column(Order = 2)]
+        public String SectionId { get; set; }
+
+        public virtual Form Form { get; set; }
+
         public virtual Section Section { get; set; }
 
         public virtual Section BelowOf { get; set; }
