11c347db482424ba5ea3c8994a3d8b80f72dd839
Thomas Miller <thomas@tlm.id.au>
2014-08-11 12:54:01 +1000

    Added functionality to add forms.

---
+++ SOFA/Infrastructure/DateTimeUtil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOFA.Infrastructure
+{
+    public class DateTimeUtil
+    {
+        public const string DATE_FORMAT = "dddd dd/MM/yyyy";
+    }
+}
