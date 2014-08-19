d1156a6dab9ab7f3c9d491d3fa97803ca29ce254
Jinivus <jinivus@jinivus.com>
2014-08-20 01:06:00 +1000

    Added SectionFieldOrder

---
+++ SOFA/Models/SectionFieldOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOFA.Models
+{
+    public class SectionFieldOrder
+    {
+        public virtual Section Section { get; set;}
+        public virtual Field Field { get; set; }
+        public byte Order { get; set; }
+
+    }
+}
