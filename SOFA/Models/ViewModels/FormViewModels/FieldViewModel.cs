ed8af686da9fef840d1ac037b23d9f143b3a0976
mitchwenman <mitchwenman@gmail.com>
2014-08-28 09:35:13 +1000

    view model conversions and unit tests

---
+++ SOFA/Models/ViewModels/FormViewModels/FieldViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOFA.Models.ViewModels.FormViewModels
+{
+    public class FieldViewModel
+    {
+        public string FieldId { get; set; }
+
+        public String FieldType { get; set; }
+
+        public String PromptValue { get; set; }
+
+        public String Value { get; set; }
+
+        public FieldViewModel()
+        {
+
+        }
+
+        public FieldViewModel(EnrolmentField field) : this()
+        {
+            FieldId = field.EnrolmentFIeldId;
+            FieldType = field.FieldType;
+            PromptValue = field.PromptValue;
+            Value = field.Value;
+        }
+    }
+}
---
+++ SOFA/Models/ViewModels/FormViewModels/SectionViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOFA.Models.ViewModels.FormViewModels
+{
+    public class SectionViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<FieldViewModel> Fields { get; set; }
+
+        public SectionViewModel()
+        {
+            Fields = new List<FieldViewModel>();
+        }
+
+        public SectionViewModel(Section section) : this()
+        {
+            Id = section.Id;
+            Name = section.Name;
+            foreach (var f in section.Fields)
+            {
+                var ef = new EnrolmentField(f);
+                Fields.Add(new FieldViewModel(ef));
+            }
+        }
+
+    }
+}
