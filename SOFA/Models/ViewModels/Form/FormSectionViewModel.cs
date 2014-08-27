a7914bc9bf3ab34f50b5c95ac32e4b9121bc7cc8
mitchwenman <mitchwenman@gmail.com>
2014-08-28 08:31:06 +1000

    added view model stubs

---
+++ SOFA/Models/ViewModels/Form/FormSectionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOFA.Models.ViewModels.Form
+{
+    public class FormSectionViewModel
+    {
+    }
+}
