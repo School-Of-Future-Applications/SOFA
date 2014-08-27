6f2a7c8d5771842c86ac61b4bcb34e960af47b9c
mitchwenman <mitchwenman@gmail.com>
2014-08-27 14:47:32 +1000

    created isvaliddate

--- SOFA/Models/Validation/EnrolmentValidator.cs
+++ SOFA/Models/Validation/EnrolmentValidator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 
@@ -17,5 +18,40 @@ namespace SOFA.Models.Validation
         {
             yield return null;
         }
+
+        #region Data Type Validation
+
+        private static Boolean IsValidDate(EnrolmentField field)
+        {
+            try
+            {
+                var culture = CultureInfo.CurrentCulture;
+                DateTime.ParseExact(field.Value, "dd/mm/yyyy", culture);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Option Validation
+
+        private static Boolean IsValidMandatory(EnrolmentField field)
+        {
+            if (field.Value.Trim().Count() > 0)
+            {
+                return true;
+            }
+
+            return false;
+            
+        }
+
+        //TODO IsValid for other option types
+
+        #endregion
     }
 }
