438d30886f6e33c57b078097ad41c8622925cf78
mitchwenman <mitchwenman@gmail.com>
2014-09-04 13:40:03 +1000

    added delete conf view model

---
+++ SOFA/Models/ViewModels/DeleteConfirmationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace SOFA.Models.ViewModels
+{
+    /**
+     * A view model for a reusable delete confirmation
+     * modal.
+     */
+    public class DeleteConfirmationViewModel
+    {
+        //The post action to be executed.
+        public string DeleteAction { get; set; }
+
+        //The controller containing the action method.
+        //Optional.
+        public string DeleteController { get; set; }
+
+        //The route dictionary values to be passed to the post method.
+        public RouteValueDictionary RouteValues { get; set; }
+
+        //The text displayed in the header of the modal.
+        public string ModalHeaderText { get; set; }
+    }
+}
