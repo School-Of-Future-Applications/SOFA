72d17ec5af455216bf71832120d56e91f8dccd02
mitchwenman <mitchwenman@gmail.com>
2014-09-07 19:07:58 +1000

    stub for esvm

---
+++ SOFA/Models/ViewModels/FormViewModels/EnrolmentSectionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SOFA.Models.ViewModels.FormViewModels
+{
+    public class EnrolmentSectionViewModel : EnrolmentSection, IValidatableObject
+    {
+        
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
