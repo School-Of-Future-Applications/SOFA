dab804d698d600f00f14acadd10161af12d07565
Thomas Miller <thomas@tlm.id.au>
2014-09-09 17:04:44 +1000

    Added view for seeing pending enrolments.
    Still not 100% finished. Iterative

--- SOFA/Models/TimetabledClass.cs
+++ SOFA/Models/TimetabledClass.cs
@@ -28,12 +28,10 @@ namespace SOFA.Models
 {
     public class TimetabledClass
     {
-
         [Key]
         public int Id { get; set; }
 
         public virtual ClassBase ClassBase { get; set; }
-
         
         public int? ClassBaseID { get; set; }
         public virtual Line Line { get; set; }
