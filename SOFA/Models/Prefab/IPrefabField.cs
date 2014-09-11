ebc3dffee19f71ba89dc386a0b5afb260df7ceef
mitchwenman <mitchwenman@gmail.com>
2014-09-11 15:07:45 +1000

    added prefab interfaces

---
+++ SOFA/Models/Prefab/IPrefabField.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOFA.Models.Prefab
+{
+    interface IPrefabField
+    {
+        public string GetId();
+
+        public string GetName();
+
+        public Field GetField();
+
+    }
+}
---
+++ SOFA/Models/Prefab/IPrefabSection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SOFA.Models;
+
+namespace SOFA.Models.Prefab
+{
+    interface IPrefabSection
+    {
+        public string GetId();
+
+        public string GetName();
+
+        public Section GetSection();
+
+    }
+}
