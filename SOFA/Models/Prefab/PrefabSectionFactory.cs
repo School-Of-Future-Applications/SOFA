0cfc144f3d303e710aa28a9662ee55a22d80a4a7
mitchwenman <mitchwenman@gmail.com>
2014-09-11 17:09:40 +1000

    prefab section factory

---
+++ SOFA/Models/Prefab/PrefabSectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SOFA.Models.Prefab.StudentDetails;
+
+namespace SOFA.Models.Prefab
+{
+    public class PrefabSectionFactory
+    {
+        public Section Get(string sectionId)
+        {
+            if (sectionId.Equals(PrefabSection.STUDENT_DETAILS))
+                return new StudentDetailsPrefabSection().GetSection();
+            else
+                throw new ArgumentException("Undefined prefab section");
+            
+        }
+    }
+}
