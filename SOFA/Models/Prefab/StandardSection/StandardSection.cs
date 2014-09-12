d99e8caebc2cd764f6f32a63baf72e706b035433
Mitch Wenman <mitchwenman@gmail.com>
2014-09-12 20:33:01 +1000

    implemented standard section

--- SOFA/Models/Prefab/StandardSection/StandardSection.cs
+++ SOFA/Models/Prefab/StandardSection/StandardSection.cs
@@ -5,7 +5,33 @@ using System.Web;
 
 namespace SOFA.Models.Prefab.StandardSection
 {
-    public class StandardSection
+    public class StandardSection : PrefabSection
     {
+        public const string NAME = "Standard Section";
+
+        private Section section;
+
+        public StandardSection()
+        {
+            this.Id = PrefabSection.STANDARD_SECTION;
+            this.Name = NAME;
+        }
+
+        public override Section GetSection()
+        {
+            if (this.section == null)
+            {
+                this.section = new Section();
+                section.Id = this.Id;
+                section.Name = this.Name;
+
+                PrefabFieldFactory factory = new PrefabFieldFactory();
+
+                section.Fields.Add(factory.Get(PrefabField.TESTFIELD_A));
+                section.Fields.Add(factory.Get(PrefabField.TESTDATEFIELD_A));                
+            }
+
+            return section;
+        }
     }
 }
