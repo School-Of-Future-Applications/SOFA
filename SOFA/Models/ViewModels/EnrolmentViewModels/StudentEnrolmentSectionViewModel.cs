0fc4eb7b7fa6ff8db28430b7e4c8d5b7a95bbedf
mitchwenman <mitchwenman@gmail.com>
2014-09-14 12:26:21 +1000

    added student enrolmentVM model and view

---
+++ SOFA/Models/ViewModels/EnrolmentViewModels/StudentEnrolmentSectionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOFA.Models.ViewModels.EnrolmentViewModels
+{
+    public class StudentEnrolmentSectionViewModel : EnrolmentSectionViewModel
+    {
+        public Student Student { get; set; }
+
+    }
+}
