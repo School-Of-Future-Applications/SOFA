95a3cae8aa00dbaba9f8ff65b82bb1d2482afafa
mitchwenman <mitchwenman@gmail.com>
2014-09-15 20:33:26 +1000

    beginnings of vm

--- SOFA/Models/ViewModels/EnrolmentViewModels/CourseEnrolmentSectionViewModel.cs
+++ SOFA/Models/ViewModels/EnrolmentViewModels/CourseEnrolmentSectionViewModel.cs
@@ -3,21 +3,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace SOFA.Models.ViewModels.EnrolmentViewModels
 {
     public class CourseEnrolmentSectionViewModel : EnrolmentSectionViewModel
     {
+        public IEnumerable<SelectListItem> DepartmentSelect { get; set; }
+
+        public IEnumerable<SelectListItem> CourseSelect { get; set; }
+
+        public IEnumerable<SelectListItem> YearLevelSelect { get; set; }
+                
         public CourseEnrolmentSectionViewModel(): base()
         { }
 
-        public CourseEnrolmentSectionViewModel(EnrolmentSection section) : base(section)
+        public CourseEnrolmentSectionViewModel(EnrolmentSection section, List<Department> departments) : base(section)
         { 
             if (section.OriginalSectionId != PrefabSection.COURSE_SELECT)
             {
                 throw new ArgumentOutOfRangeException("Enrolment Section not a course select section");
             }
+            DepartmentSelect = new SelectList(departments, "Id", "Name");
+
+            //At this stage just display the departments
+            CourseSelect = new SelectList(Enumerable.Empty<SelectListItem>());
+            YearLevelSelect = new SelectList(Enumerable.Empty<SelectListItem>());
+
         }
+
+
 
 
     }
