4b594cfb6c859126a034f50398bd51976ba73f34
mitchwenman <mitchwenman@gmail.com>
2014-09-15 20:17:26 +1000

    constructors

--- SOFA/Models/ViewModels/EnrolmentViewModels/CourseEnrolmentSectionViewModel.cs
+++ SOFA/Models/ViewModels/EnrolmentViewModels/CourseEnrolmentSectionViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SOFA.Models.Prefab;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,17 @@ namespace SOFA.Models.ViewModels.EnrolmentViewModels
 {
     public class CourseEnrolmentSectionViewModel : EnrolmentSectionViewModel
     {
+        public CourseEnrolmentSectionViewModel(): base()
+        { }
+
+        public CourseEnrolmentSectionViewModel(EnrolmentSection section) : base(section)
+        { 
+            if (section.OriginalSectionId != PrefabSection.COURSE_SELECT)
+            {
+                throw new ArgumentOutOfRangeException("Enrolment Section not a course select section");
+            }
+        }
+
+
     }
 }
