c2be1a9325a66feafd654eafd9aac94dcfc52936
mitchwenman <mitchwenman@gmail.com>
2014-09-16 13:46:05 +1000

    added js file

---
+++ SOFA/Scripts/class-select.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+
+    //Select List handlers
+    $(".department-select").change(function () {
+        var deptId = $(this).val();
+        if (deptId != "") {
+            //Get courses and append to course select
+        }
+    });
+
+    $(".course-select").change(function () {
+        console.log("Course Select")
+    });
+
+    $(".yearlevel-select").change(function () {
+        console.log("Year Select")
+    });
+});
