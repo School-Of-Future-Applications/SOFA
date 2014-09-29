de5609ee3138e19c04707fd88c186e2f6eb5d437
mitchwenman <mitchwenman@gmail.com>
2014-09-29 19:13:51 +1000

    removed section field from model

--- SOFA/Models/SectionFieldOrder.cs
+++ SOFA/Models/SectionFieldOrder.cs
@@ -9,11 +9,8 @@ namespace SOFA.Models
 {
     public class SectionFieldOrder
     {
-        [Key, ForeignKey("Section"), Column(Order=0)]
-        public string SectionID { get; set; }
         [Key, ForeignKey("Field"), Column(Order = 1)]
         public string FieldID { get; set; }
-        public virtual Section Section { get; set;}
         public virtual Field Field { get; set; }
         public int Order { get; set; }
 
