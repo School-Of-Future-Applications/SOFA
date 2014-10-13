28cf2f0963f1c401592b458188e696d04d8f7fa0
Jinivus <jinivus@jinivus.com>
2014-10-13 22:53:32 +1000

    Fixes Terminal-Coding/SOFA#259

--- SOFA/Controllers/FileController.cs
+++ SOFA/Controllers/FileController.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SOFA.Infrastructure;
+using SOFA.Models;
 
 namespace SOFA.Controllers
 {
-    public class UploadController : Controller
+    public class FileController : Controller
     {
 
         [HttpPost]
@@ -25,6 +26,12 @@ namespace SOFA.Controllers
             return null;
         }
 
+        public FileResult GetFile(string fileId)
+        {
+            DBContext dbCon = new DBContext();
+            var f = dbCon.Files.Where(x=> x.FileID==fileId).FirstOrDefault();
+            return File(FileManager.GetFile(fileId), System.Net.Mime.MediaTypeNames.Application.Octet, "SOFADownload"+System.IO.Path.GetExtension(f.Location));
+        }
 
         //
         // GET: /Upload/
