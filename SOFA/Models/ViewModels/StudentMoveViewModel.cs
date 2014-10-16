127aa08de7b9ceb1edb961bb89249bbd1484453b
mitchwenman <mitchwenman@gmail.com>
2014-10-16 17:15:39 +1000

    created student move view model

---
+++ SOFA/Models/ViewModels/StudentMoveViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOFA.Models.ViewModels
+{
+    public class StudentMoveViewModel
+    {
+        public int CurrentTimetabledClassId { get; set; }
+
+        public List<String> EnrolmentFormIds { get; set; }
+
+        public int NewTimetabledClassId { get; set; }
+
+        public SelectList TimetabledClasses { get; set; }
+ 
+
+        /* Default constructor */
+        public StudentMoveViewModel()
+        { }
+
+        public StudentMoveViewModel(ICollection<TimetabledClass> Classes) : this()
+        {
+            TimetabledClasses = new SelectList(Classes, "Id", "DisplayName");
+        }
+    }
+}
